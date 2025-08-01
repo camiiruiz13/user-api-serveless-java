eea24139a8371ee6850b762bc4edb068df92840c
camiiruiz13 <69661273+camiiruiz13@users.noreply.github.com>
2025-08-01 16:35:50 -0500

    Se crea modelos y dto para la clases

---
+++ src/main/java/com/aws/ccamilo/com/app/useapiserveless/domain/serviices/IUserServices.java
@@ -0,0 +1,8 @@
+package com.aws.ccamilo.com.app.useapiserveless.domain.serviices;
+
+import com.aws.ccamilo.com.app.useapiserveless.domain.model.User;
+
+public interface IUserServices {
+
+    void saveUser(User user);
+}
