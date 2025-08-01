50c3db35c1b0c7b095007aa02c03526e97040d3e
camiiruiz13 <69661273+camiiruiz13@users.noreply.github.com>
2025-08-01 16:57:24 -0500

    Creacion de servicio estructura de datos

---
+++ src/main/java/com/aws/ccamilo/com/app/useapiserveless/domain/services/impl/UserServices.java
@@ -0,0 +1,4 @@
+package com.aws.ccamilo.com.app.useapiserveless.domain.services.impl;
+
+public class UserServices {
+}
--- src/main/java/com/aws/ccamilo/com/app/useapiserveless/domain/serviices/IUserServices.java
+++
@@ -1,8 +0,0 @@
-package com.aws.ccamilo.com.app.useapiserveless.domain.serviices;
-
-import com.aws.ccamilo.com.app.useapiserveless.domain.model.User;
-
-public interface IUserServices {
-
-    void saveUser(User user);
-}
